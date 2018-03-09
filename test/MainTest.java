0f35df1d3a299380d7d7a29a706c1769d049c5ad
Vadym <vadympopovychn24@gmail.com>
2018-03-09 14:06:21 +0200

    1_run_gradle

---
+++ test/MainTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+    @Test
+    public void isDelim() {
+    }
+
+    @Test
+    public void isOperator() {
+    }
+
+    @Test
+    public void priority() {
+    }
+
+    @Test
+    public void processOperator() {
+    }
+
+    @Test
+    public void eval() {
+    }
+
+    @Test
+    public void livesum() {
+    }
+
+    @Test
+    public void mathematic() {
+    }
+
+    @Test
+    public void vkl() {
+    }
+
+    @Test
+    public void vykl() {
+    }
+
+    @Test
+    public void main() {
+    }
+}
